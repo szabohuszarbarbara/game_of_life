dd5f6be1a4fa050e05fc8f43a5ab4ebfa23440d7
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-04-11 11:15:20 +0200

    add base board, not sizeable yet

--- game_of_life.js
+++ game_of_life.js
@@ -19,3 +19,44 @@
     // A megjelölt sejtek eltávolítása
 
 
+
+function drawBoard(row, col) {
+    const board = document.createElement("div")
+    board.setAttribute("class", "board")
+    board.setAttribute("class", "board-container")
+
+    for (let i = 0; i < row; i++) {
+        let row = document.createElement("div")
+        row.setAttribute("class", "row")
+        row.setAttribute("data-row", `${i}`)
+        for (let j = 0; j < col; j ++) {
+            let cell = document.createElement("div")
+            cell.setAttribute("class", "cell")
+            cell.setAttribute("data-row", `${i}`)
+            cell.setAttribute("data-col", `${j}`)
+            cell.addEventListener("click", setLivingCells)
+            row.appendChild(cell)
+        }
+        board.appendChild(row)
+    }
+    const body = document.querySelector("body")
+    body.appendChild(board)
+}
+
+function setLivingCells(clickEvent) {
+    const cell = clickEvent.currentTarget
+    cell.classList.add("living")
+
+
+}
+
+
+function init() {
+    drawBoard(20, 20)
+    alert("Please set the living cells by clicking on them! When you finished click OK!")
+    setLivingCells()
+    // startGame()
+}
+
+
+init()
