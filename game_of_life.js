6164e15ce283e82719d2b3482c16b2bcbd2636cd
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-04-11 10:56:02 +0200

    start project with overview

---
+++ game_of_life.js
@@ -0,0 +1,21 @@
+// game menu
+//         graphics
+//         start game button
+//         some explanation
+
+// adjust board size custom vs full screen
+
+// add init cells
+
+// start game / stop / pause / restart / quit
+
+// rules:
+//     A sejt túléli a kört, ha két vagy három szomszédja van.
+//     A sejt elpusztul, ha kettőnél kevesebb (elszigetelődés), vagy háromnál több (túlnépesedés) szomszédja van.
+//     Új sejt születik minden olyan cellában, melynek környezetében pontosan három sejt található.
+
+    // Az elhaló sejtek megjelölése
+    // A születő sejtek elhelyezése
+    // A megjelölt sejtek eltávolítása
+
+
